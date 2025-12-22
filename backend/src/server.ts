f17f889f63501d7b5cd1c94a9db2e3a7bdd5478e
Ronaldo Davi <ronaldodavi@gmail.com>
2025-12-22 11:26:42 -0300

    fix: resolve frontend login crash and update microservices

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,8 +2,10 @@ import gracefulShutdown from "http-graceful-shutdown";
 import app from "./app";
 import { initIO } from "./libs/socket";
 import { logger } from "./utils/logger";
-import { StartAllWhatsAppsSessions } from "./services/WbotServices/StartAllWhatsAppsSessions";
+import { EventListener } from "./services/WbotServices/EventListener";
 import RabbitMQService from "./services/RabbitMQService";
+import { StartAllWhatsAppsSessions } from "./services/WbotServices/StartAllWhatsAppsSessions";
+import { CommandListener } from "./services/WbotServices/CommandListener";
 
 const startServer = async () => {
   await RabbitMQService.connect();
@@ -13,6 +15,8 @@ const startServer = async () => {
   });
 
   initIO(server);
+  await EventListener();
+  await CommandListener();
   StartAllWhatsAppsSessions();
   gracefulShutdown(server);
 };
